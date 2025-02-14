0ef139b03ab778b0b34b42121364848c1802d8ed
Drashti Fefar <63639707+Patel-7777@users.noreply.github.com>
2025-02-14 10:38:55 +0530

    Made connection to postgres, celery and clerybeat in a project (#3)
    Co-authored-by: Drashti Fefar

--- youtube_video_fetcher/youtube_video_fetcher/settings.py
+++ youtube_video_fetcher/youtube_video_fetcher/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -37,6 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "django_celery_beat",
+    "videos",
 ]
 
 MIDDLEWARE = [
@@ -69,17 +73,40 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "youtube_video_fetcher.wsgi.application"
 
-
+REST_FRAMEWORK = {
+    "DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",),
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": 5432,
+    },
 }
 
+# Celery Configuration
+CELERY_BROKER_URL = (
+    f"{os.getenv('MQ_PROTOCOL')}://{os.getenv('MQ_USER')}:{os.getenv('MQ_PASSWORD')}"
+    f"@{os.getenv('MQ_HOST')}:{os.getenv('MQ_PORT')}/"
+)
+CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
+CELERY_BROKER_HEARTBEAT = 3600
+CELERY_TASK_DEFAULT_QUEUE = "default"
+CELERY_WORKER_CANCEL_LONG_RUNNING_TASKS_ON_CONNECTION_LOSS = True
+CELERY_RESULT_BACKEND = (
+    "redis://"
+    f":{os.getenv('REDIS_PASSWORD')}"
+    f"@{os.getenv('REDIS_HOST')}:{os.getenv('REDIS_PORT')}/{os.getenv('REDIS_DB')}"
+)
+CELERY_BEAT_SCHEDULE = {}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
