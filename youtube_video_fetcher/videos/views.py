c3c2c8d76a477f7b31c70f7df2631065ac35f42b
Drashti Fefar <drashti@gopronto.io>
2025-02-14 11:20:38 +0530

    Api to search video based on title and description.

--- youtube_video_fetcher/videos/views.py
+++ youtube_video_fetcher/videos/views.py
@@ -7,3 +7,11 @@ from .serializers import VideoSerializer
 class VideoListView(ListAPIView):
     queryset = Video.objects.all().order_by('-published_datetime')
     serializer_class = VideoSerializer
+
+
+class VideoSearchView(ListAPIView):
+    serializer_class = VideoSerializer
+
+    def get_queryset(self):
+        query = self.request.query_params.get('query', '')
+        return Video.objects.filter(title__icontains=query) | Video.objects.filter(description__icontains=query)
